70176acbea647b8d3bf2b8a5832b3aedae454988
joaovitormv <joaovitorjjabuti@gmail>
2023-09-15 10:38:28 -0300

    foi

---
+++ app/Validators/RegisterUserValidator.ts
@@ -0,0 +1,29 @@
+import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class RegisterUserValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  
+  public schema = schema.create({
+    name: schema.string({}, [
+      rules.required()
+    ]),
+    email: schema.string({}, [
+      rules.required(),
+      rules.email(),
+      rules.unique({ table: 'users', column: 'email'})
+    ]),
+    password: schema.string({}, {
+      rules.required(),
+      rules.minLength(4)
+    })
+  })
+
+
+  public messages: CustomMessages = {
+    required: "0 {{field}} é obrigatório para se registrar!!!",
+    'email.unique': "E-mail já cadastrado!!!",
+    'minLenght': "Tamanho de senha inválida"
+  }
+}
