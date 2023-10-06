c515f6e5deaeb39a9ab1cd23a767ff4edbaec146
joaovitormv <126258486+joaovitormv@users.noreply.github.com>
2023-10-06 07:31:16 -0300

    Update AuthController.ts

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -5,7 +5,7 @@ import RegisterUserValidator from 'App/Validators/RegisterUserValidator'
 export default class AuthController {
 
     public async register({ request }: HttpContextContract){
-        const data = await request.validate(RegisterUserValidator)
+        const data = await request.validate(RegisterUserValidator) /*Usa o RegisterUserValidator validar os dados enviados; como o await, ele só passa pra linha de baixo quando dá certo */
         const user = await User.create(data)
         return user
     }
@@ -13,13 +13,13 @@ export default class AuthController {
     public async login({request, auth, response}: HttpContextContract){
         try{
             const { email, password } = request.all()
-            const token = await auth.use('api').attempt(email, password, {
+            const token = await auth.use('api').attempt(email, password, { /*Usa o sistema de autenticação api, tenta autenticar o usuário com base no email e password recebidos; se der certo, ele gera um token.
                 expiresIn: '1day'
             })
-            const user = await User.findByOrFail("email", email)
+            const user = await User.findByOrFail("email", email) /*Verifica no banco de dados se esse email existe*/
             return { token, user}
         }catch (error){
-            response.status(401).send("Login ou senha incorretos!")
+            response.status(401).send("Login ou senha incorretos!") /*se alguma coisa der errado, ele retorna essa mensagem de erro*/
         }
     }
 
